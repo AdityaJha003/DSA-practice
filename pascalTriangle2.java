11e486cf8c830ff8c371cc7f9566ad21ffd007e5
AdityaJha <jha.aditya1608@gmail.com>
2024-07-17 20:29:44 +0530

    pascalTrianle2

---
+++ pascalTriangle2.java
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/pascals-triangle-ii/
+// Input: rowIndex = 3
+// Output: [1,3,3,1]
+
+import java.util.Arrays;
+import java.util.List;
+
+public class pascalTriangle2 {
+    public List<Integer> getRow(int rowIndex) {
+    Integer[] ans = new Integer[rowIndex + 1];
+    Arrays.fill(ans, 1);
+
+    for (int i = 2; i < rowIndex + 1; ++i)
+      for (int j = 1; j < i; ++j)
+        ans[i - j] += ans[i - j - 1];
+
+    return Arrays.asList(ans);
+  }
+  public static void main(String[] args) {
+    System.out.println("Pascal Triangle 2");
+  }
+}
