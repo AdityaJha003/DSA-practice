b4b4397fd2972fe9606522ac71c2e01901c48d01
AdityaJha <jha.aditya1608@gmail.com>
2024-07-31 20:12:53 +0530

    longestRepeatingChrReplace

---
+++ longestRepeatingChrReplace.java
@@ -0,0 +1,22 @@
+public class longestRepeatingChrReplace {
+    public int characterReplacement(String s, int k) {
+        int ans = 0;
+        int maxCount = 0;
+        int[] count = new int[26];
+    
+        for (int l = 0, r = 0; r < s.length(); ++r) {
+          maxCount = Math.max(maxCount, ++count[s.charAt(r) - 'A']);
+          while (maxCount + k < r - l + 1)
+            --count[s.charAt(l++) - 'A'];
+          ans = Math.max(ans, r - l + 1);
+        }
+    
+        return ans;
+      }
+      public static void main(String[] args) {
+        longestRepeatingChrReplace obj = new longestRepeatingChrReplace();
+        String s = "AABABBA";
+        int k = 1;
+        System.out.println(obj.characterReplacement(s, k));
+      }
+}
