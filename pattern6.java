8fb0958bae38a35a7420e245891d2769f051fb96
AdityaJha <jha.aditya1608@gmail.com>
2024-08-20 15:40:52 +0530

    pattern6

---
+++ pattern6.java
@@ -0,0 +1,21 @@
+public class pattern6 {
+    public static void main(String[] args) {
+        p(5);
+    }
+
+    static void p (int n){
+         for(int row = 0; row < 2*n; row++){
+            int totalColsInRow = row > n ? 2*n - row : row;
+            
+            int noOfSpaces = n - totalColsInRow;
+            for(int s = 0; s < noOfSpaces; s++){
+                System.out.print(" ");
+            }
+
+            for(int col = 0; col < totalColsInRow; col++){
+                System.out.print("* ");
+            }
+            System.out.println();
+         }
+    }
+}
